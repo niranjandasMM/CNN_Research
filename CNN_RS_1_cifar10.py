1f465953c35c25e9cb6850c9cc652f4117fb256a
Victoris Galaxía <85221814+niranjandasMM@users.noreply.github.com>
2022-04-07 11:42:47 +0530

    cnn_from_scratch
    dataset used : cifar10 , Reseacrh 1 | getting random accuracy , test accuracy approx. 50%

---
+++ CNN_RS_1_cifar10.py
@@ -0,0 +1,231 @@
+import tensorflow as tf 
+import matplotlib.pyplot as plt
+import skimage.data  
+import numpy as np
+import skimage.data 
+from sklearn.utils import shuffle
+
+cifar10 = tf.keras.datasets.cifar10
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+x_train = skimage.color.rgb2gray(x_train[0:50000])
+x_test = skimage.color.rgb2gray(x_test[0:10000])
+
+class Conv3x3:
+  # A Convolution layer using 3x3 filters.
+
+  def __init__(self, num_filters):
+    self.num_filters = num_filters
+
+    self.filters = np.random.randn(num_filters, 3, 3) / 9
+
+  def iterate_regions(self, image):
+
+    h, w = image.shape
+
+    for i in range(h - 2):
+      for j in range(w - 2):
+        im_region = image[i:(i + 3), j:(j + 3)]
+        yield im_region, i, j
+
+  def forward(self, input):
+
+    self.last_input = input
+
+    h, w = input.shape
+    output = np.zeros((h - 2, w - 2, self.num_filters))
+
+    for im_region, i, j in self.iterate_regions(input):
+      output[i, j] = np.sum(im_region * self.filters, axis=(1, 2))
+
+    return output
+
+  def backprop(self, d_L_d_out, learn_rate):
+
+    d_L_d_filters = np.zeros(self.filters.shape)
+
+    for im_region, i, j in self.iterate_regions(self.last_input):
+      for f in range(self.num_filters):
+        d_L_d_filters[f] += d_L_d_out[i, j, f] * im_region
+
+    # Update filters
+    self.filters -= learn_rate * d_L_d_filters
+
+    return None
+    
+class MaxPool2:
+  # A Max Pooling layer using a pool size of 2.
+
+  def iterate_regions(self, image):
+
+    h, w, _ = image.shape
+    new_h = h // 2
+    new_w = w // 2
+
+    for i in range(new_h):
+      for j in range(new_w):
+        im_region = image[(i * 2):(i * 2 + 2), (j * 2):(j * 2 + 2)]
+        yield im_region, i, j
+
+  def forward(self, input):
+
+    self.last_input = input
+
+    h, w, num_filters = input.shape
+    output = np.zeros((h // 2, w // 2, num_filters))
+
+    for im_region, i, j in self.iterate_regions(input):
+      output[i, j] = np.amax(im_region, axis=(0, 1))
+
+    return output
+
+  def backprop(self, d_L_d_out):
+
+    d_L_d_input = np.zeros(self.last_input.shape)
+
+    for im_region, i, j in self.iterate_regions(self.last_input):
+      h, w, f = im_region.shape
+      amax = np.amax(im_region, axis=(0, 1))
+
+      for i2 in range(h):
+        for j2 in range(w):
+          for f2 in range(f):
+            # If this pixel was the max value, copy the gradient to it.
+            if im_region[i2, j2, f2] == amax[f2]:
+              d_L_d_input[i * 2 + i2, j * 2 + j2, f2] = d_L_d_out[i, j, f2]
+
+    return d_L_d_input
+
+class Softmax:
+  # A standard fully-connected layer with softmax activation.
+
+  def __init__(self, input_len, nodes):
+    # We divide by input_len to reduce the variance of our initial values
+    self.weights = np.random.randn(input_len, nodes) / input_len
+    self.biases = np.zeros(nodes)
+
+  def forward(self, input):
+
+    self.last_input_shape = input.shape
+
+    input = input.flatten()
+    self.last_input = input
+
+    input_len, nodes = self.weights.shape
+
+    totals = np.dot(input, self.weights) + self.biases
+    self.last_totals = totals
+
+    exp = np.exp(totals)
+    return exp / np.sum(exp, axis=0)
+
+  def backprop(self, d_L_d_out, learn_rate):
+
+    # We know only 1 element of d_L_d_out will be nonzero
+    for i, gradient in enumerate(d_L_d_out):
+      if gradient == 0:
+        continue
+
+      # e^totals
+      t_exp = np.exp(self.last_totals)
+
+      # Sum of all e^totals
+      S = np.sum(t_exp)
+
+      # Gradients of out[i] against totals
+      d_out_d_t = -t_exp[i] * t_exp / (S ** 2)
+      d_out_d_t[i] = t_exp[i] * (S - t_exp[i]) / (S ** 2)
+
+      # Gradients of totals against weights/biases/input
+      d_t_d_w = self.last_input
+      d_t_d_b = 1
+      d_t_d_inputs = self.weights
+
+      # Gradients of loss against totals
+      d_L_d_t = gradient * d_out_d_t
+
+      # Gradients of loss against weights/biases/input
+      d_L_d_w = d_t_d_w[np.newaxis].T @ d_L_d_t[np.newaxis]
+      d_L_d_b = d_L_d_t * d_t_d_b
+      d_L_d_inputs = d_t_d_inputs @ d_L_d_t
+
+      # Update weights / biases
+      self.weights -= learn_rate * d_L_d_w
+      self.biases -= learn_rate * d_L_d_b
+
+      return d_L_d_inputs.reshape(self.last_input_shape)
+
+conv = Conv3x3(32)                  
+pool = MaxPool2()                  
+softmax = Softmax(15 * 15 * 8, 10) # 13x13x8 -> 10
+
+def forward(image, label):
+
+  # We transform the image from [0, 255] to [-0.5, 0.5] to make it easier
+  # to work with. This is standard practice.
+  out = conv.forward(image)
+  out = pool.forward(out)
+  out = softmax.forward(out)
+
+  # Calculate cross-entropy loss and accuracy. np.log() is the natural log.
+  loss = -np.log(out[label])
+  acc = 1 if np.argmax(out) == label else 0
+
+  return out, loss, acc
+
+def train(im, label, lr=.005):
+
+  # Forward
+  out, loss, acc = forward(im, label)
+
+  # Calculate initial gradient
+  gradient = np.zeros(10)
+  gradient[label] = -1 / out[label]
+
+  # Backprop
+  gradient = softmax.backprop(gradient, lr)
+  gradient = pool.backprop(gradient)
+  gradient = conv.backprop(gradient, lr)
+
+  return loss, acc
+
+print('MNIST CNN initialized!')
+
+# Train the CNN for 3 epochs
+for epoch in range(3):
+  print('--- Epoch %d ---' % (epoch + 1))
+
+  # Shuffle the training data
+  permutation = np.random.permutation(len(x_train))
+  train_images = x_train[permutation]
+  train_labels = y_train[permutation]
+
+  # Train!
+  loss = 0
+  num_correct = 0
+  for i, (im, label) in enumerate(zip(x_train, y_train)):
+    if i % 100 == 99:
+      print(
+        '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
+        (i + 1, loss / 100, num_correct)
+      )
+      loss = 0
+      num_correct = 0
+
+    l, acc = train(im, label)
+    loss += l
+    num_correct += acc
+
+# Test the CNN
+print('\n--- Testing the CNN ---')
+loss = 0
+num_correct = 0
+for im, label in zip(x_test, y_test):
+  _, l, acc = forward(im, label)
+  loss += l
+  num_correct += acc
+
+num_tests = len(x_test)
+print('Test Loss:', loss / num_tests)
+print('Test Accuracy:', num_correct / num_tests)
